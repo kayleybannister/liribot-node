229473909da01fb8998b86f3284bb1f0e65d6564
Kayley Bannister <kayley.bannister@gmail.com>
2019-08-07 20:05:43 -0400

    got the concert-this command working and created functions instead of if else statements for the commands

--- liri.js
+++ liri.js
@@ -1,9 +1,9 @@
 //adding code to read / set any environment variables with the dotenv package
 require("dotenv").config();
 
-var dotenv = require("dotenv");
+var moment = require("moment");
 
-//Adding code required to import the `keys.js` file and store it in a variable.
+//Adding code required to import the `keys.js` file and store it in a variable
 var keys = require("./keys.js");
 
 var Spotify = require("node-spotify-api");
@@ -12,12 +12,67 @@ var spotify = new Spotify(keys.spotify);
 // Axios code
 var axios = require("axios");
 
-var liriRequest = process.argv[2];
-var selection = process.argv[3];
+//===========================================================================================
 
-if (liriRequest === "movie-this") {
+app( process.argv[2], process.argv[3] );
+
+//===========================================================================================
+
+function app(command, params) {
+
+    switch (command) {
+
+        case "concert-this":
+            showBand(params);
+            break;
+    
+        case "movie-this":
+            showMovie(params);
+            break;
+    
+        case "spotify-this-song":
+            showSong(params);
+            break;
+    
+        case "do-what-it-says":
+            doIt();
+            break;
+    
+        default:
+                console.log("ERROR - This command is not recognized." + "\nPlease select one of the below commands:" +
+                "\nmovie-this spotify-this-song do-what-it-says concert-this");
+            break;
+    }
+};
+
+function showBand(artists)
+{
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artists + "/events?app_id=codingbootcamp";
+
+    axios.get(queryUrl).then(
+        function(response){
+            if (!response.data.length)
+            {
+                console.log("No result found for " + artists);
+                return;
+            };
+
+            console.log("Upcoming concerts for " + artists + ": ");
+            for (let i = 0; i < response.data.length; i++) {
+                const show = response.data[i];
+
+                console.log(show.venue.city + ", " + (show.venue.region || show.venue.country) + " at " + 
+                show.venue.name + " " + moment(show.datetime).format("hh:mm a, MM/DD/YYYY"));
+                console.log("====================================== \n");
+            };
+        }
+    );
+};
+
+// if the liriRequest is equal to movie-this, Axios calls OMDB to search for the movie and return specific info
+function showMovie(movie) {
 // We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=trilogy").then(
+axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
   function(response) {
     // Then we print out the imdbRating
     console.log("TITLE: " + response.data.Title);
@@ -28,29 +83,18 @@ axios.get("http://www.omdbapi.com/?t=" + selection + "&y=&plot=short&apikey=tril
     console.log("PLOT: " + response.data.Plot);
     console.log("ACTORS: " + response.data.Actors);
   }
-)}
+)};
 
-else if (liriRequest === "concert-this")
-{
-    //run code for Bands in Town API to display location and venue for this artist
-    //Need Bands in Town API - requested
-    console.log("CONCERT FOR: " + selection);
-}
 
-else if (liriRequest === "spotify-this-song")
-{
+function showSong(song) {
+    
     //run code to Spotify API to display artist info
-    spotify.get("")
-    console.log("PLAY THIS SONG: " + selection);
-}
-
-else if (liriRequest === "do-what-it-says") 
-{
-    //run code to add to random.txt file
-    console.log("YOU TOLD ME TO: " + selection);
-}
-
-else {
-    console.log("ERROR - This command is not recognized." + "\nPlease select one of the below commands:" +
-    "\nmovie-this spotify-this-song do-what-it-says concert-this");
-}
+    spotify.search({type: "track", query: song}, function(err, data)
+    {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+        console.log(data.artist);
+    });
+    console.log("PLAY THIS SONG: " + song);
+};
